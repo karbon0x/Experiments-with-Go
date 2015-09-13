339877754b8012cce43ca1ca8d6d580a78c7f10c
Rohan Seth <rseth2@illinois.edu>
2015-09-13 13:00:27 -0500

    Trying to do an RPC call to 8 servers

--- client.go
+++ client.go
@@ -1,50 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/rpc"
-    "os"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
 )
 
 type Fin struct {
-	T,T2 string
+	T, T2 string
 }
 
 func main() {
-    if len(os.Args) != 2 {
-        fmt.Println("Usage: ", os.Args[0], "server")
-        os.Exit(1)
-    }
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ", os.Args[0], "server")
+		os.Exit(1)
+	}
 
-    serverAddress := os.Args[1]
-    client, err := rpc.Dial("tcp", serverAddress+":1234")
-    if err != nil {
-        log.Fatal("dialing:", err)
-    }
-    var input string
-       fmt.Scanln(&input)
-       fmt.Print(input)
-       var input2 string
-       fmt.Scanln(&input2)
-       fmt.Print(input2)
-      var A[]byte
+	serverAddress := os.Args[1]
+	client, err := rpc.Dial("tcp", serverAddress+":1234")
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	var input string
+	fmt.Scanln(&input)
+	fmt.Print(input)
+	var input2 string
+	fmt.Scanln(&input2)
+	fmt.Print(input2)
+	var A []byte
 
-
-
-arg := Fin{input,input2}
-    err = client.Call("Arith2.Readi", arg, &A)
-if err != nil {
-     log.Fatal("Read error", err)
- }
-
-
-fmt.Println(string(A))
-str := strings.Split(string(A),"\n")
-for i := 0; i < len(str); i++ {
-    if strings.Contains(str[i], "ftp"){
-            fmt.Println(str[i])
-        }
-    }
+	arg := Fin{input, input2}
+	err = client.Call("Arith2.Readi", arg, &A)
+	if err != nil {
+		log.Fatal("Read error", err)
+	}
+	i
 }
--- server.go
+++ server.go
@@ -1,34 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net"
 	"net/rpc"
-	"log"
-	"fmt"
 	//"io/ioutil"
 	"os/exec"
 )
 
 type Fin struct {
-	T,T2 string
+	T, T2 string
 }
 
 type Arith2 []byte
 
-
-func (t *Arith2) Readi(arg* Fin, reply *[]byte) error{
+func (t *Arith2) Readi(arg *Fin, reply *[]byte) error {
 	var err error
 	fmt.Println(arg.T2)
 
 	cmd := exec.Command("grep", arg.T2, arg.T)
 
 	*reply, err = cmd.Output()
-    //data, err := ioutil.ReadFile(arg.T)
-    if err!=nil{
-        return nil
-    }
+	//data, err := ioutil.ReadFile(arg.T)
+	if err != nil {
+		return nil
+	}
 	//*reply = data
-    return nil
+	return nil
 }
 
 func main() {
@@ -37,13 +36,13 @@ func main() {
 	rpc.Register(arith2)
 	l, e := net.Listen("tcp", ":1234")
 	if e != nil {
-	    log.Fatal("listen error:", e)
+		log.Fatal("listen error:", e)
 	}
-	    for {
-	        conn, err := l.Accept()
-			 if err != nil {
-			    log.Fatal("listen error:", err)
-			}
-			rpc.ServeConn(conn)
-	    }
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal("listen error:", err)
+		}
+		rpc.ServeConn(conn)
+	}
 }
